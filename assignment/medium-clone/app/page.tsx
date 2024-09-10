4903fb2dd27003209b9f472806d77bde1e840657
HiyaKashyap99 <hiyakashyap2004@gmail.com>
2024-09-10 21:54:02 +0530

    changes

--- assignment/medium-clone/app/page.tsx
+++ assignment/medium-clone/app/page.tsx
@@ -1,15 +1,37 @@
-import Header from '../components/Header'
-import Banner from '../components/Banner'
-import Footer from '../components/Footer'
+import Header from '../components/Header';  
+import Banner from '../components/Banner';  
+import PostCard from '../components/PostCard';  
+
+const styles = {
+  container:'max-w-7xl flex-1',
+  main:'flex justify-center',
+  wrapper:'mx-auto',
+  postsList: 'flex flex-col gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3',
+};
+
 export default function Home() {
-  const styles = {}
   return (
     <>
-    <Header/>
-    <Banner/>
-    <Footer/>
+    <div className='styles.wrpper'>
+      <Header />   
+      <Banner />  
+      <div className={styles.main}>
+      <div className={styles.container}>
+      <div className={styles.postsList}>
+        <PostCard />  
+        <PostCard />
+        <PostCard />
+      </div>
+      </div> 
+      </div>
+      </div>
     </>
+<<<<<<< HEAD
     
     )
 }
 fvf
+=======
+  );
+}
+>>>>>>> d1a4b56 (changes)
--- assignment/medium-clone/components/Banner.js
+++ assignment/medium-clone/components/Banner.js
@@ -2,19 +2,28 @@ import Image from 'next/image';
 import ban from '../images/banner.webp';
 
 const styles = {
+<<<<<<< HEAD
     banner: 'flex flex-col md:flex-row items-center justify-between p-10 bg-[#D6DF7E]', 
     heading: 'max-w-xl text-[6rem] font-mediumSerifItalic ', 
     subheading: 'text-2xl mt-0', 
     accentedButton: 'bg-green-600 text-black py-2 px-4 rounded-full cursor-pointer hover:bg-green-700 mt-6',
     imageContainer: 'flex justify-end mt-6 md:mt-0', 
     image: 'max-w-full h-auto'
+=======
+  banner: 'flex flex-col md:flex-row items-center justify-between p-10 bg-[#D6DF7E]', 
+  heading: 'max-w-xl text-[6rem] font-mediumSerifItalic', 
+  subheading: 'text-2xl mt-0', 
+  accentedButton: 'bg-green-600 text-black py-2 px-4 rounded-full cursor-pointer hover:bg-green-700 mt-6',
+  imageContainer: 'flex justify-end mt-6 md:mt-0 hidden md:flex', // Hidden on small screens, flex on md and larger
+  image: 'max-w-full h-auto'
+>>>>>>> d1a4b56 (changes)
 };
 
 const Banner = () => {
   return (
     <div className={styles.banner}>
       <div className='space-y-5 px-10 flex-[3]'>
-        <h1 className={styles.heading}>Human stories& ideas</h1>
+        <h1 className={styles.heading}>Human stories & ideas</h1>
         <h3 className={styles.subheading}>A place to read, write, and deepen your understanding</h3>
         <button className={styles.accentedButton}>Start Reading</button>
       </div>
